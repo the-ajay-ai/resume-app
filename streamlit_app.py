a67b970b8001db2c01dfceb15f3f5420c49ec9e2
Ajay <33282854+the-ajay-ai@users.noreply.github.com>
2022-02-19 18:38:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,9 +32,6 @@ st.markdown("""
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
       <li class="nav-item">
         <a class="nav-link" href="#education">Education</a>
       </li>
